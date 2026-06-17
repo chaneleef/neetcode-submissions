d527627f199c08a1a119b7815ddc891cd1a401ee
chaneleef <145681837+chaneleef@users.noreply.github.com>
2026-06-17 10:20:05 -0400

    Update: is-palindrome - submission-0

--- Data Structures & Algorithms/is-palindrome/submission-0.py
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -1,9 +1,17 @@
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        newStr = ""
-        for c in s: 
-            if c.isalnum():
-                newStr += c.lower()
-        return newStr == newStr[::-1]
+        import re
+        l = 0 
+        
+        
+        cleaned_text = re.sub(r'[^A-Za-z0-9]', '', s)
+        cleaned_text = cleaned_text.lower()
+        r = len(cleaned_text)-1 
 
-                 
+        while l < r:
+            if cleaned_text[l] == cleaned_text[r]:   
+                l += 1
+                r -= 1
+            else:
+                return False 
+        return True 
